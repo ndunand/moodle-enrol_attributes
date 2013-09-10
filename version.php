5c24fc488a86475278778ee826a9be4c2c43c4f9
Nicolas Dunand <nico@kolya.ch>
2013-09-10 07:35:22 +0200

    Updated version.php

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2013050100;
+$plugin->version = 2012080100;
 $plugin->requires  = 2011120100;
 $plugin->release = 'Version 1.0 (build 2013050100)';
 $plugin->maturity  = MATURITY_STABLE;
