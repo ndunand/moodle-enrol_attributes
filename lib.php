a6f7205d19f4284606925451cebaa1383db9cf1e
Nicolas Dunand <nico@kolya.ch>
2015-10-12 16:00:22 +0200

    Implemented new mandatory Moodle 2.9 functions

--- lib.php
+++ lib.php
@@ -66,6 +66,28 @@ class enrol_attributes_plugin extends enrol_plugin {
     }
 
     /**
+     * Is it possible to delete enrol instance via standard UI?
+     *
+     * @param object $instance
+     * @return bool
+     */
+    public function can_delete_instance($instance) {
+        $context = context_course::instance($instance->courseid);
+        return has_capability('enrol/attributes:config', $context);
+    }
+
+    /**
+     * Is it possible to hide/show enrol instance via standard UI?
+     *
+     * @param stdClass $instance
+     * @return bool
+     */
+    public function can_hide_show_instance($instance) {
+        $context = context_course::instance($instance->courseid);
+        return has_capability('enrol/attributes:config', $context);
+    }
+
+    /**
      * Returns edit icons for the page with list of instances
      * @param stdClass $instance
      * @return array
