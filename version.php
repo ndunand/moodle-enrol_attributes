03ef99c1f4f0e41903c75fdcac120df7d736e34d
Nicolas Dunand <nico@kolya.ch>
2015-10-13 16:39:36 +0200

    Updated copyright information

--- version.php
+++ version.php
@@ -1,5 +1,4 @@
 <?php
-
 // This file is part of Moodle - http://moodle.org/
 //
 // Moodle is free software: you can redistribute it and/or modify
@@ -16,9 +15,9 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * @package    enrol
- * @subpackage attributes
- * @copyright  2012 Copyright Université de Lausanne, RISET {@link http://www.unil.ch/riset}
+ * @package    enrol_attributes
+ * @author     Nicolas Dunand <Nicolas.Dunand@unil.ch>
+ * @copyright  2012-2015 Université de Lausanne (@link http://www.unil.ch}
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
