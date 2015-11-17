3f72414805b8b4a1fcfc51eebf89e16a55ef5842
Nicolas Dunand <nico@kolya.ch>
2015-11-17 13:19:30 +0100

    Official Moodle 3.0 support

--- version.php
+++ version.php
@@ -23,10 +23,10 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2015101301;
+$plugin->version = 2015111700;
 $plugin->requires  = 2014051200; // Moodle 2.7
 $plugin->component = 'enrol_attributes';
-$plugin->release = '2.0.1 for Moodle 2.7-2.9 (build 2015101301)';
+$plugin->release = '2.1 for Moodle 2.7-3.0 (build 2015111700)';
 $plugin->maturity  = MATURITY_STABLE;
 
 $plugin->cron = 3600 * 6; // every 6 hours
