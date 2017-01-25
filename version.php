d204e378fdc5516d6060b5f33ed79eb0f041a321
Nicolas Dunand <nico@kolya.ch>
2017-01-25 14:55:32 +0100

    New version for release

--- version.php
+++ version.php
@@ -23,10 +23,10 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2016120100;
+$plugin->version = 2017012500;
 $plugin->requires = 2014051200; // Moodle 2.7
 $plugin->component = 'enrol_attributes';
-$plugin->release = '2.4 for Moodle 2.7-3.2 (build 2016120100)';
+$plugin->release = '2.4.1 for Moodle 2.7-3.2 (build 2017012500)';
 $plugin->maturity = MATURITY_STABLE;
 
 $plugin->cron = 3600 * 6; // every 6 hours
