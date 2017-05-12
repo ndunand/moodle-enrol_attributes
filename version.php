74621611ff92f55d178a7926bc592b2d5bc5701a
Nicolas Dunand <nico@kolya.ch>
2017-05-12 13:25:47 +0200

    Bumped up version.php for Moodle 3.3

--- version.php
+++ version.php
@@ -23,10 +23,10 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2017012500;
+$plugin->version = 2017050100;
 $plugin->requires = 2014051200; // Moodle 2.7
 $plugin->component = 'enrol_attributes';
-$plugin->release = '2.4.1 for Moodle 2.7-3.2 (build 2017012500)';
+$plugin->release = '2.4.2 for Moodle 2.7-3.3 (build 2017050100)';
 $plugin->maturity = MATURITY_STABLE;
 
 $plugin->cron = 3600 * 6; // every 6 hours
