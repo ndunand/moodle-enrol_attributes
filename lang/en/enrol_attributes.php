577d776084d2ee6f23a8bc667c352776ed1d0a1a
Nicolas Dunand <nico@kolya.ch>
2021-06-30 08:23:01 +0200

    Fix for https://github.com/ndunand/moodle-enrol_attributes/issues/35

--- lang/en/enrol_attributes.php
+++ lang/en/enrol_attributes.php
@@ -58,4 +58,5 @@ $string['whenexpiredremove'] = 'Unenroll user';
 $string['whenexpiredsuspend'] = 'Suspend user';
 $string['observelogins'] = 'Enrol users immediately at login';
 $string['observelogins_desc'] = 'Try to enrol users immediately when they log in. This can have a performance impact on your site, deactivate this if lots of users log in at the same time and their being enrolled at once becomes a bottleneck.';
+$string['cachedef_dbquerycache'] = 'DB query cache';
 
