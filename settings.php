57950332b18bf64c025156e8e985bc1aeaa988de
Nicolas Alexandropoulos <Nicolas.Alexandropoulos@unil.ch>
2022-12-23 14:44:02 +0100

    Fix the no custom field warning display in settings

--- settings.php
+++ settings.php
@@ -58,10 +58,9 @@ if ($ADMIN->fulltree) {
         $settings->add(new admin_setting_configmultiselect('enrol_attributes/profilefields',
                 get_string('profilefields', 'enrol_attributes'), get_string('profilefields_desc', 'enrol_attributes'),
                 [], $customfields));
-    } else if (!defined('PHPUNIT_TEST')) {
+    } else if (!PHPUNIT_TEST) {
         // The warning needs to be given only if unit tests are not running.
         // Otherwise some core tests might fail.
-        $url = new moodle_url('/user/profile/index.php');
         \core\notification::warning(get_string('no_custom_field', 'enrol_attributes',$CFG->wwwroot . '/user/profile/index.php'));
     }
 
